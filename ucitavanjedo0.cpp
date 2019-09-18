c51bd9365fbd8e8667fb5d6757bdea98a7f9d5a6
frankecerin <55490490+frankecerin@users.noreply.github.com>
2019-09-18 14:21:17 +0200

    Add files via upload

---
+++ ucitavanjedo0.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+//Ucitavanje brojeva dok se ne ucita 0
+
+int main() {
+	
+	int a;
+	
+	cout<<"Unesi broj:"<<endl;
+	cin>> a;
+	
+	while (a!=0)
+	cin>> a;
+ 
+		
+	
+	return 0;
+	
+}
