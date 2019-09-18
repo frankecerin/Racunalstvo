52e6977b2df631713a05517812e2dc294eb0d3bb
frankecerin <55490490+frankecerin@users.noreply.github.com>
2019-09-18 14:16:34 +0200

    Add files via upload

---
+++ 0do1000po7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+//Program koji ispisuje cijele brojeve od 0 do 1000 sa korakom 7
+
+int main() {
+	
+	int vrijeme;
+	
+	int brojac;
+	
+	for(brojac=vrijeme;brojac<1000;brojac+=7){
+		cout<<brojac<<endl;
+		Sleep(500);
+		
+		
+	}
+	
+	
+	return 0;
+}
