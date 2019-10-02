17541e522ae756efc009ea527b1ac42065493946
Fran Kecerin <55490490+frankecerin@users.noreply.github.com>
2019-10-02 14:17:45 +0200

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+	
+	int x,y;
+	
+	cout<<"Unesite broj clanova niza:"<<endl;
+	cin>>x;
+	
+	cout<<"Unesite clanove niza (broj od kojeg niz pocinje):"<<endl;
+	cin>>y;
+	
+	int program[x];
+	for(x;y<x;y++ ){
+		cout<<y<<endl;
+	}
+		
+		
+		
+	return 0;
+}
