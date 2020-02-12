2a827216781b3ec3400f5e03de9d581b861a394d
Fran Kecerin <55490490+frankecerin@users.noreply.github.com>
2020-02-12 14:07:58 +0100

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string sastavi(string A, string B, string C) {
+       return A + B + C;
+       }
+
+int main()
+{
+  string prvi;
+  string drugi;
+  string treci;
+  cout<<"Unesi tri rijeci: (pritisni ENTER nakon unosa svake rijeci.)"<<endl;
+  getline(cin, prvi);
+  getline(cin, drugi);
+  getline(cin, treci);
+  cout<<sastavi(prvi,drugi,treci);
+  cout<<endl;
+  
+  
+    system("PAUSE");
+    return 0;
+}
